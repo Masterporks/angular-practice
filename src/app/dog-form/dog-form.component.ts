0b38ee49bac81dfbf8be83d0040624cd6afb0e29
root <joosepkorela@gmail.com>
2023-02-13 21:10:11 +0200

    Models examples added

---
+++ src/app/dog-form/dog-form.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {Dog} from "../shared/models/dog";
+import {NgForm} from "@angular/forms";
+
+@Component({
+  selector: 'app-dog-form',
+  templateUrl: './dog-form.component.html',
+  styleUrls: ['./dog-form.component.css']
+})
+export class DogFormComponent {
+
+  dogs: Dog[] = [];
+  name: string = '';
+  type: string = '';
+  property: string = '';
+
+isShowForm: boolean = false;
+  addDog(dogForm: NgForm) {
+    this.dogs.push(new Dog(this.name, this.type, this.property));
+    dogForm.reset();
+  }
+}
